da66550d00e45eed086810c8feeded49043186b2
EduardsZ <eduards.zvaigzne@gmail.com>
2020-07-09 14:42:31 +0300

    Modified inbox.php

--- inbox.php
+++ inbox.php
@@ -5,12 +5,4 @@ error_reporting(E_ALL);
 
 header('Content-Type: application/json');
 
-if(file_exists('inboxWeather.json') and filemtime('inboxWeather.json') > time() - 300) {
-  echo file_get_contents('inboxWeather.json');
-} else {
-  $info = file_get_contents('https://www.inbox.lv/api/weather');
-  file_put_contents('inboxWeather.json', $info);
-  file_put_contents('cond.txt', 'File update time: ' . date('Y-m-d H-i-s', filemtime('inboxWeather.json')) . "\n");
-  file_put_contents('cond.txt', 'Script call time: ' . date('Y-m-d H-i-s', time() ), FILE_APPEND);
-  echo $info;
-}
+require_once $_SERVER['DOCUMENT_ROOT'] . '/../src/inbox.php';
